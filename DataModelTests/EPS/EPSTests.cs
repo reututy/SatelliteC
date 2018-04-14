4809ad112273d62e90531c4abb1f53bc687420a6
mor_lavi <lavi.mor@gmail.com>
2018-04-14 13:14:25 +0300

    added DataModelTests project

---
+++ DataModelTests/EPS/EPSTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataModel.EPS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.EPS.Tests
+{
+    [TestClass()]
+    public class EPSTests
+    {
+        [TestMethod()]
+        public void EPSTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GET_HK_1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GET_HK_2Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GET_HK_2_VITest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GET_HK_2_OUTTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
